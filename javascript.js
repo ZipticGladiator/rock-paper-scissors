6e930601c95e9b4789e321d73502dda06f8fa6ab
Siya <siya.gladile@gmail.com>
2022-12-25 19:58:40 +0200

    added js functions

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function getComputerChoice() {
+
+}
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+function game(playRound){
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+     }
+}
